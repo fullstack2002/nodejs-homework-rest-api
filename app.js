56ed23a350b824e03210a7fe74db29ba3589653e
fullstack2002 <98652392+fullstack2002@users.noreply.github.com>
2023-01-12 00:10:17 +0200

    bugfix

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
 require("dotenv").config();
-const multer = require("multer");
-const path = require("path");
-const fs = require("fs/promises");
 
 const authRouter = require("./routes/api/auth");
 const contactsRouter = require("./routes/api/contacts");
@@ -13,34 +10,10 @@ const app = express();
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
-const contacts = [];
-
-const tempDir = path.join(__dirname, "temp");
-
-const multerConfig = multer.diskStorage({
-  destination: tempDir,
-})
-
-const upload = multer({
-  storage: multerConfig,
-})
-
-app.get("/api/contacts", (req, res) => {
-  res.json(contacts);
-})
-
-const contactsDir = path.join(__dirname, "public", "contacts");
-app.post("/api/contacts", upload.single("cover"), async (req, res) => {
-  const { path: tempUpload, originalname } = req.file;
-  const resultUpload = path.join(contactsDir, originalname);
-  await fs.rename(tempUpload, resultUpload);
-})
-
-app.listen(2000);
-
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 app.use("/api/auth", authRouter);
 app.use("/api/contacts", contactsRouter);
---
+++ controllers/auth/updateAvatar.js
@@ -0,0 +1,26 @@
+const fs = require("fs/promises")
+const path = require("path")
+const jimp = require("jimp");
+
+const { User } = require("../../models/db/user")
+
+const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
+
+const updateAvatar = async (req, res) => {
+  const { path: tempUpload, originalname } = req.file;
+  const { _id } = req.user;
+  const image = await jimp.read(tempUpload);
+  await image.resize(250, 250);
+  await image.writeAsync(tempUpload);
+  const filename = `${_id}_${originalname}`;
+  const resultUpload = path.join(avatarsDir, filename);
+  await fs.rename(tempUpload, resultUpload);
+  const avatarUrl = path.join("avatars", filename);
+  await User.findByIdAndUpdate(_id, { avatarUrl });
+
+  res.json({
+    avatarURL,
+  })
+}
+
+module.exports = updateAvatar;
